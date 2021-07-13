3f4af988dfdf0572327e01b53e664c9cc81a4e35
StormShadow24k <shaurya2407k@gmail.com>
2021-07-13 10:13:52 +0530

    fixed quotes and added embeds? ig

---
+++ embeds/EvelinEmbedBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace Evelin.Embeds
+{
+    using Discord;
+    using Discord.WebSocket;
+
+    /// <summary>
+    /// A custom embed builder to prevent repetitive code.
+    /// </summary>
+    internal class EvelinEmbedBuilder : EmbedBuilder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EvelinEmbedBuilder"/> class.
+        /// </summary>
+        public EvelinEmbedBuilder()
+        {
+            this.WithColor(new Color(148, 54, 178));
+        }
+    }
+}
--- modules/General.cs
+++ modules/General.cs
@@ -1,10 +1,11 @@
 ﻿namespace Evelin.Modules
 {
-    using System;
     using System.Net.Http;
-    using System.Text.Json;
     using System.Threading.Tasks;
+    using Discord;
     using Discord.Commands;
+    using Discord.WebSocket;
+    using Evelin.Embeds;
     using Newtonsoft.Json.Linq;
 
     /// <summary>
@@ -21,7 +22,9 @@
         public async Task PingAsync()
         {
             await this.Context.Channel.TriggerTypingAsync();
-            await this.Context.Channel.SendMessageAsync("Pong!");
+            await this.Context.Channel.SendMessageAsync($"Pong! ms");
+
+            // add latency command ask on discord for DiscordSocketClient.Latency(); or whatever it is
         }
 
         /// <summary>
@@ -38,9 +41,14 @@
             JArray qarray = JArray.Parse(result);
             string quote = qarray[0]["q"].ToString();
             string author = qarray[0]["a"].ToString();
-            await this.ReplyAsync($"{quote} - {author}");
+            var embed = new EvelinEmbedBuilder()
+                .WithTitle("Quote")
+                .WithDescription($"{quote} - {author}")
+                .WithFooter(this.Context.Client.CurrentUser.GetAvatarUrl() ?? this.Context.Client.CurrentUser.GetDefaultAvatarUrl(), $"{this.Context.Client.CurrentUser.Username}#{this.Context.Client.CurrentUser.Discriminator}")
+                .Build();
+            await this.ReplyAsync(embed: embed);
 
-            // put embed
+            // check this command out once its a disaster
         }
     }
 }
